843dc29496002d94539a51e3b8340356e181b51f
Igor Zubkov <igor.zubkov@gmail.com>
2025-12-06 16:18:02 +0100

    Fix test

--- spec/routing/up_routing_spec.rb
+++ spec/routing/up_routing_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-RSpec.describe Rails::HealthController do
+RSpec.describe Rails::HealthController, type: :routing do
   describe "routing" do
     it "should route GET /up to rails/health#show" do
       expect(get: "/up").to route_to(
